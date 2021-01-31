f62fa674fcdf70a27d647545390db84bc34ff7f4
Federico Hermann <hermann.f.a@gmail.com>
2021-01-31 11:59:56 -0300

    cambios Entrada Salida 03 y 04

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,10 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let dato = prompt("Ingrese Dato");
+		
+	txtIdNombre.value = dato;
+
+	alert(txtIdNombre.value);
 }
 
