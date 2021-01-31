9200322d6122c12db13e6380cae1fba47f2a567a
Federico Hermann <hermann.f.a@gmail.com>
2021-01-31 16:29:36 -0300

    Cambios EntradaSalida-09 porcentaje adicional

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,16 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	//declaro las variables y reservo espacio en la memoria
+	let sueldo;
+	let resultado;
 
-	alert("ok");
+	//Asignamos el valor del input "sueldo" en la variable correspondiente
+	sueldo = parseInt(document.getElementById("txtIdSueldo").value);
+
+	//calculamos el aumento y lo asignamos a otra variable
+	resultado = sueldo * 1.10
+	
+	//ponemos el resultado en el cuadro de texto solicitado
+	document.getElementById("txtIdResultado").value = resultado;
 }
