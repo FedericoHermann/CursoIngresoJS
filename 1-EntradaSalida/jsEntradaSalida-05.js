10fc7329c5850873a3664146ced0f9c42a6f1533
Federico Hermann <hermann.f.a@gmail.com>
2021-01-31 14:34:39 -0300

    cambios en EntradaSalida 05 (html y JS)

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -3,6 +3,17 @@ Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+	//reservo memoria para alojar las variables correspondientes a los datos necesarios
+	let nombre;
+	let edad;
+
+		//tomo el valor recavado por el user input de nombre en la variable "nombre"
+		nombre = document.getElementById("txtIdNombre").value;
+
+		//tomo el valor recavado por el user input de edad en la variable "edad"
+		edad = document.getElementById("txtIdEdad").value;
+	
+		//pone en pantalla mediante un cuadro de diálogo el valor de ambas variables dentro de una cadena de texto
+	alert("Su nombre es"+" "+nombre+" "+"y su edad es"+" "+edad);
 }
 
