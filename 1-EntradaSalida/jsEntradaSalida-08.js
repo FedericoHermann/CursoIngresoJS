69feb359d07db6c5894d5a957a4b1a04eae4bc87
Federico Hermann <hermann.f.a@gmail.com>
2021-01-31 16:18:00 -0300

    Cambios en EntradaSalida-08 con división por cero

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -5,5 +5,23 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	//declaro las variables y reservo espacio en la memoria
+	let dividendo;
+	let divisor;
+	let resto;
+
+	//Asigno valores a traves del input del usuario a cada variable
+	dividendo = parseInt(document.getElementById("txtIdNumeroDividendo").value);
+	divisor = parseInt(document.getElementById("txtIdNumeroDivisor").value);
+	
+	// construyo un condicional para excluir la posibilidad de división por 0
+	if (divisor === 0) {
+		resto = "división por cero no admitida";
+	} else {
+		//Preparo la operación de las variables dividendo y divisor, dentro del valor de la variable resto
+		resto = dividendo % divisor;
+	}
+		
+	//Pongo el resultado en pantalla
+	alert(`El Resto es ${resto}`);
 }
