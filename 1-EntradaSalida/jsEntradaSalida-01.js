280e5afc83edf9184a842e863733fd7bed2b1857
Federico Hermann <fhermann@alfalince.local>
2021-02-01 19:16:34 -0300

    añadido nombre y apellido

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,6 +1,11 @@
 /*
+Hermann Federico
+
 Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"
+
+Ejercicio 01*/
+
 function mostrar()
 {
 	alert("Esto funciona de maravilla");
