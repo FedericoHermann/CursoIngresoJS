6b0a24bde834bfd6004438c7ae62120382a2f99a
Federico Hermann <hermann.f.a@gmail.com>
2021-02-02 21:03:46 -0300

    Completo IF 01

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,23 @@
 function mostrar()
 {
+
+	//declaro variables
+	let edad;
+
 	//tomo la edad  
-	alert("ok");
+	
+	edad = document.getElementById("txtIdEdad").value;
+
+	edad = parseInt(edad);
+
+	//Publico la edad en pantalla
+
+	if (edad === 15) {
+		alert(`Niña Bonita!`)
+	} else {
+		alert(`Su edad es: ${edad} años.`)
+	}
+
+	
 
 }//FIN DE LA FUNCIÓN
