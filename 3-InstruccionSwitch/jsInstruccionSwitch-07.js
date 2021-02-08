5a354d7824054d705a93cebaf261d731b3f08cc8
Federico Hermann <hermann.f.a@gmail.com>
2021-02-08 20:43:37 -0300

    Switch 07

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,35 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	
+	//Declaro Variables
+	let destino;
+
+	//asigno valores
+	destino = document.getElementById("txtIdDestino").value;
+
+	//Calculo condición
+
+	switch (destino) {
+		case "Bariloche":
+			alert(`Punto Cardinal: Oeste`)
+			break;
+		
+		case "Cataratas":
+			alert(`Punto Cardinal: Norte`)
+			break;
+
+		case "Mar del plata":
+			alert(`Punto Cardinal: Este`)
+			break;
+		
+		case "Ushuaia":
+			alert(`Punto Cardinal: Sur`)
+			break;
+					
+		default:
+			break;
+	}
+
+	
 
 }//FIN DE LA FUNCIÓN
