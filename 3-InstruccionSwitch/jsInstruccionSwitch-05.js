d65cb9f55f6f2dd30e4dca6f877f82434b6df4c5
Federico Hermann <hermann.f.a@gmail.com>
2021-02-08 18:59:44 -0300

    Switch 05

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -1,8 +1,28 @@
 function mostrar()
 {
-	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
+	//declaro variable
+	let hora;
+	
+	//Asigno valor
+	hora = document.getElementById("txtIdHora").value;
+	hora = parseInt(hora);
+
+	//Preparo condición
+	
+	switch (hora) {
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+			alert("Es de mañana")		
+		break;
+	
+		default:
+			break;
+	}
+
+	
 	
 	
 
