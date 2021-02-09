82c0d8d91e7e717a0c0b0dbecf8d8a26e5eaa351
Federico Hermann <fhermann@alfalince.local>
2021-02-09 19:40:18 -0300

    func While 01 y 02

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -3,5 +3,21 @@ al presionar el botón mostrar 10 repeticiones
 con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
 {
-	alert('iteración while');
+
+//delcaro variables
+let ascendente;
+
+//Otorgo valor para controlar el orden ascendente
+ascendente = 1;
+
+//armo el loop
+	while (ascendente < 11) //evaluo el valor de ascendente
+	{
+		alert(`El numero es ${ascendente}`); //publico en pantalla el valor obtenido
+		
+		ascendente++; //aumento el valor de ascendente
+  	}
+
+
+	
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -3,8 +3,21 @@ al presionar el botón mostrar 10 repeticiones
 con números DESCENDENTES, desde el 10 al 1.*/
 function mostrar()
 {
-	var contador;
-	contador=0; 
-	alert('iteración while');
 
+	//delcaro variables
+	let descendente;
+
+	//Otorgo valor para controlar el orden ascendente
+	descendente = 10;
+
+	//armo el loop
+		while (descendente > 0) //evaluo el valor de ascendente
+		{
+			alert(`El numero es ${descendente}`); //publico en pantalla el valor obtenido
+			
+			descendente--; //disminuyo el valor de descendente
+		}
+
+
+		
 }//FIN DE LA FUNCIÓN
