297453e6b1927ae8fa5300eb6fb81171c28bd60a
Federico Hermann <hermann.f.a@gmail.com>
2021-02-08 21:51:52 -0300

    Switch 10

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,57 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	//declaro variables
+	let estacion;
+	let destino;
+
+	//asigno valores
+	estacion = document.getElementById("txtIdEstacion").value; 
+	destino = document.getElementById("txtIdDestino").value;
+
+	//evaluo condiciones
+
+	if (estacion == "Invierno") { //evaluamos invierno
+		switch (destino) {
+			case "Bariloche":
+				alert(`Se Viaja`);
+				break;
+		
+			default:
+				alert(`No se Viaja`);
+				break;
+		}
+	} else {
+		if (estacion == "Verano") { //evaluamos verano
+			switch (destino) {
+				case "Cataratas":
+				case "Mar del plata":
+					alert(`Se Viaja`);
+					break;
+			
+				default:
+					alert(`No se Viaja`);
+					break;
+			}
+			
+		} else {
+			if (estacion == "Otoño") { //evaluamos Otoño
+				alert(`Se Viaja`)
+				
+			} else {
+				switch (destino) {
+					case "Bariloche":
+						alert(`No se Viaja`);
+						break;
+				
+					default:
+						alert(`Se Viaja`)
+						break;
+				}
+			}
+		}
+	}
+
+
+
 
 }//FIN DE LA FUNCIÓN
