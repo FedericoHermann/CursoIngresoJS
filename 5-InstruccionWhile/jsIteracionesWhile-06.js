0533e20793162ca43ec8460f2008912c6eee8933
Federico Hermann <hermann.f.a@gmail.com>
2021-02-11 21:04:43 -0300

    While 06

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,30 @@
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var numeroIngresado;
+	//declaro variables
+	let contador;
+	let acumulador;
+	let numeroIngresado;
 
+	//asigno valores iniciales
 	contador=0;
-	acumulador=0;
-	
-	
+	acumulador=0; 
+
+	//hago loop de conteo
+	while (contador <5)
+	{
+		contador ++; //asigno posición de número
+
+		numeroIngresado = prompt(`Ingrese su número nº${contador}`) //tomo valor del usuario
+
+		numeroIngresado = parseFloat(numeroIngresado); //parseo el valor ingresado
+
+		acumulador = acumulador + numeroIngresado; //sumo el valor ingresado a la variable "acumulador"
+	}
+
+	// suma de todos los número ingresados
 	txtIdSuma.value=acumulador;
+
+	//promedio de todos los números ingresados
 	txtIdPromedio.value=acumulador/5;
+
 }//FIN DE LA FUNCIÓN
