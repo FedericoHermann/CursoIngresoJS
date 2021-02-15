4937d445dca40ade6741b653e36fc573aed70ce5
Federico Hermann <hermann.f.a@gmail.com>
2021-02-15 17:12:13 -0300

    TP04, da NaN en cantidad=4

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -10,69 +10,67 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
-     
-    //defino variables
-    let cantidad;
+    //defino variables a utilizar.
     let marca;
-    let precioOrig;
+    let cantidad;
     let descuento;
-    let precioFinal;
+    let precio;
     let iibb;
+    let total;
 
-    //asigno valores a las cantidades
-    cantidad = document.getElementById("txtIdCantidad").value;
-    cantidad = parseInt(cantidad);
+    //tomo datos del usuario y los cargo en las variables correspondientes.
 
-    //asigno valores a la elección de "marca"
-    marca = document.getElementById("Marca").value;
+    marca = document.getElementById("Marca").value; //tomo la marca como valor "string" del dropdown menu.
+    cantidad = document.getElementById("txtIdCantidad").value; //tomo la cantidad ingresada de lamparas.
+    cantidad = parseInt(cantidad); //parseo cantidad para convertir su contenido en un número entero.
+
+    /* asigno el precio por unidad.
+    Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.*/
+
+    precio = 35; 
     
+    //con los valores esenciales tomados, pasamos a la etapa de cálculo.
 
-    //establezco el precio original
-    precioOrig = 35;
-    descuento = 1;
 
-    //Bloque de código para condiciones de venta a aplicar según cantidad y marca.
-    if (cantidad >= 6) 
+    if (cantidad >= 6)
+    //A.Si compra 6 o más lamparitas bajo consumo tiene un descuento del 50%.
     {
-         //A.Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
-         descuento = 0.5;  
-            
+       descuento = 0.5; 
     } 
     else 
     {
-        
         switch (cantidad) 
         {
-            /*B.Si compra 5 lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % 
-            y si es de otra marca el descuento es del 30%.*/
             case 5:
+                /*B.Si compra 5 lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % 
+                y si es de otra marca el descuento es del 30%.*/
                 if (marca == "ArgentinaLuz") 
                 {
-                    descuento = 0.4;                    
+                    descuento = 0.4;
                 } 
                 else 
                 {
-                    descuento = 0.3;   
+                   descuento = 0.3; 
                 }
                 break;
 
-            /*C.Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” 
-            se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.*/
             case 4:
+                /*C.Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace 
+                un descuento del 25 % y si es de otra marca el descuento es del 20%.*/
                 if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") 
-                {
-                    descuento = 0.25;            
+                { 
+                    decuento = 0.25;
                 } 
                 else 
                 {
-                    descuento = 0.2;
-                }      
+                    decuento = 0.2;
+                }
                 break;
 
-            /*D. Si compra 3 lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, 
-            si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.*/
             case 3:
-                if (marca == "ArgentinaLuz") 
+                /*Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  
+                “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.*/
+                if (marca == "ArgentinaLuz")
                 {
                     descuento = 0.15;
                 } 
@@ -80,51 +78,42 @@ function CalcularPrecio ()
                 {
                     if (marca == "FelipeLamparas") 
                     {
-                        descuento = 0.1;                
+                        descuento = 0.1;
                     } 
                     else 
                     {
-                        descuento = 0.05;            
+                        descuento = 0.05;
                     }
+                   
                 }
                 break;
-                        
+                
             default:
-                precioFinal = precioOrig*cantidad;
+
+                descuento = 0;
+
                 break;
         }
+        
 
-        alert(descuento);
-    }
-   
-    precioFinal = precioFinal*descuento; //aplico descuentos
-    
+    }  
+
+    //calculamos el importe total
+    total = (precio - (precio*descuento))*cantidad;
+
+        /*E.Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos 
+        en informar del impuesto con el siguiente mensaje:”Usted pago X de IIBB.”, siendo X el impuesto que se pagó.*/ 
+        if (total > 120) 
+        {
+            iibb = total * 0.1; //cargo el valor de los Ingresos Brutos a la variable.
+            alert(`Usted pagó ${iibb} de IIBB`); //Informo el importe del impuesto en pantalla.
+
+            //Modifico el valor total con los Ingresos Brutos sumados.
+            total = total + iibb;
+        } 
 
 
-    if (precioFinal > 120) 
-    { //calculo IIBB
-        iibb = precioFinal * 0.10; 
-        precioFinal = precioFinal + iibb;
-        document.getElementById("txtIdprecioDescuento").value = precioFinal.toFixed(2);
-        alert(`Usted pago $${iibb.toFixed(2)} en concepto de IIBB `);
-    } 
-    else 
-    {
-        document.getElementById("txtIdprecioDescuento").value = precioFinal.toFixed(2); 
-    }
+    //finalmente enviamos el valor obtenido al cuadro de texto
+    document.getElementById("txtIdprecioDescuento").value = total;
 
 }
-    
-   
-    
-       
-            
-
-
-
-       
-    
-
-    
-
-
