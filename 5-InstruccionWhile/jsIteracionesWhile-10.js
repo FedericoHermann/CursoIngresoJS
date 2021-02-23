9d52c9e03f3da5b425751026ad3f83f988dde305
Federico Hermann <hermann.f.a@gmail.com>
2021-02-23 19:49:27 -0300

    Arreglos en while 10

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -25,7 +25,6 @@ function mostrar()
 	let promedioNegativos;
 	let diferencia;
 
-
 	//Asigno valores iniciales
 	respuesta="si";
 	sumaNegativos=0;
@@ -35,54 +34,67 @@ function mostrar()
 	contadorCeros=0;
 	contadorPares=0;
 
-
-	while(respuesta=="si")
+	while(respuesta =="si")
 	{	
 		numeroIngresado = prompt(`Ingrese un número:`); //pido valor
 		numeroIngresado = parseInt(numeroIngresado);  //parseo valor
 
-		if (numeroIngresado %2 == 0 ) 
-		{
-			contadorPares ++; //aumenta el contador de pares
-		}
+			if (numeroIngresado %2 == 0 ) 
+			{
+				contadorPares ++; //aumenta el contador de pares
+			}
 
-		if (numeroIngresado < 0 ) 
-		{
-			sumaNegativos = sumaNegativos + numeroIngresado; //aumenta la suma de negativos
-
-			contadorNegativos ++; //aumenta el contador de negativos
-		}
-
-		if (numeroIngresado = 0 ) 
-		{
-			contadorCeros ++; //aumenta el contador de ceros
-		}
-
-		if (numeroIngresado > 0 ) 
-		{
-			sumaPositivos = sumaPositivos + numeroIngresado; //aumenta la suma de negativos
-
-			contadorPositivos ++; //aumenta el contador de negativos
-		}
+			if (numeroIngresado < 0 ) 
+			{
+				sumaNegativos = sumaNegativos + numeroIngresado; //aumenta la suma de negativos
+				contadorNegativos ++; //aumenta el contador de negativos
+			}
+			else
+			{	if (numeroIngresado >0) 
+				{
+					sumaPositivos = sumaPositivos + numeroIngresado; //aumenta la suma de positivos
+					contadorPositivos ++; //aumenta el contador de positivos
+				}
+				else
+				{
+					contadorCeros ++; //aumenta el contador de ceros
+				}
+			}
 
 		respuesta=prompt("desea continuar?", "si/no");
 		respuesta = respuesta.toLocaleLowerCase()
-		
+
 	}//fin del while
 
-	//Calculamos promedios y diferencia
-	promedioPositivos = sumaPositivos / contadorPositivos;
-	promedioNegativos = sumaNegativos / contadorNegativos;
+	//Calculamos promedios y diferencia, sorteamos el no ingreso de números de algún tipo
+	if (contadorPositivos !=0 ) 
+	{
+		promedioPositivos = sumaPositivos / contadorPositivos;
+	} 
+	else 
+	{
+		promedioPositivos = "No se ingresaron números positivos";
+	}
+	
+	if (promedioNegativos != 0 ) 
+	{
+		promedioNegativos = sumaNegativos / contadorNegativos;
+	} 
+	else 
+	{
+		promedioNegativos = "No se ingresaron números negativos";
+	}
+	
 	diferencia = sumaPositivos - sumaNegativos;
 
-
-	document.write("la suma de Negativos es :"+sumaNegativos);
-	document.write("la suma de Positivos es :"+sumaPositivos);
-	document.write("la cantidad de negativos es :"+contadorNegativos);
-	document.write("la cantidad de positivos es :"+contadorPositivos);
-	document.write("la cantidad de ceros es :"+contadorCeros);
-	document.write("la cantidad de pares es :"+contadorPares);
-	document.write("el promedio de los positivos es :"+promedioPositivos);
-	document.write("el promedio de los negativos es :"+promedioNegativos);
-	document.write("la diferencia entre positivos y negativos es :"+diferencia);
+	//document Write
+	document.write("La suma de Negativos es :"+sumaNegativos+"<br>");
+	document.write("La suma de Positivos es :"+sumaPositivos+"<br>");
+	document.write("La cantidad de negativos es :"+contadorNegativos+"<br>");
+	document.write("La cantidad de positivos es :"+contadorPositivos+"<br>");
+	document.write("La cantidad de ceros es :"+contadorCeros+"<br>");
+	document.write("La cantidad de pares es :"+contadorPares+"<br>");
+	document.write("El promedio de los positivos es :"+promedioPositivos+"<br>");
+	document.write("El promedio de los negativos es :"+promedioNegativos+"<br>");
+	document.write("La diferencia entre positivos y negativos es :"+diferencia+"<br>");
 }//FIN DE LA FUNCIÓN
