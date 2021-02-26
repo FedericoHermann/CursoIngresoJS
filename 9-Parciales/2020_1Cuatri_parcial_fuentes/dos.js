7508e4ec0e5156ce23460dbdb4e4c43ba3e7551f
Federico Hermann <hermann.f.a@gmail.com>
2021-02-25 22:22:44 -0300

    ejercicios de parcial 2020 funcionando

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,178 @@
+/* Parcial 2020
+Ejercicio 02
+
+Hermann Federico */
+
 function mostrar()
 {
-  alert("dos");
-}
+  //declaro variables
+
+  let tipoProducto;
+  let cantidadBolsas;
+  let precioBolsa;
+  let respuesta;
+  let importeTotalBruto;
+  let importeTotalDescuento;
+  let descuento;
+  let contadorArena;
+  let contadorCal;
+  let contadorCemento;
+  let caroArena;
+  let caroCal;
+  let caroCemento;
+  let totalBolsas;
+  
+
+  //Asigno valores iniciales 
+  caroArena = 0;
+  caroCal = 0;
+  caroCemento = 0;
+  contadorArena = 0;
+  contadorCal = 0;
+  contadorCemento = 0;
+  importeTotalBruto = 0;
+  importeTotalDescuento = 0;
+  respuesta = "si";
+
+
+  //Realizo bloque de validaciones
+
+  while ( respuesta == "si" ) 
+  {
+    tipoProducto = prompt(`Ingrese el tipo de producto deseado`, `arena / cal / cemento`); // Solicito producto
+    tipoProducto = tipoProducto.toLowerCase();
+
+    while ( isNaN(tipoProducto) == false || tipoProducto != "arena" && tipoProducto != "cal" && tipoProducto != "cemento") 
+    {
+      tipoProducto = prompt(`Error de Ingreso, Reingrese el tipo de producto deseado`, `arena / cal / cemento`); // Solicito producto
+      tipoProducto = tipoProducto.toLowerCase();
+    }
+
+    cantidadBolsas = prompt(`Ingrese la cantidad de Bolsas`, `ej: 2`); //solicito cantidad de productos
+    cantidadBolsas = parseFloat(cantidadBolsas);
+
+    while ( isNaN(cantidadBolsas) == true || cantidadBolsas < 0 )
+    {
+      cantidadBolsas = prompt(`Error en cantidad de Productos, reingrese la cantidad de Bolsas`, `ej: 2`); //solicito cantidad de productos
+      cantidadBolsas = parseFloat(cantidadBolsas);
+    }
+
+    precioBolsa = prompt(`Ingrese el precio por Bolsa de Producto`, `valor debe ser mayor que 0, Ej: 100`); //Solicito precio
+    precioBolsa = parseFloat(precioBolsa);
+
+    while ( isNaN(precioBolsa) == true || precioBolsa < 0 ) 
+    {
+      precioBolsa = prompt(`Error de Precio, Reingrese el precio de su producto`, "valor debe ser mayor que 0, Ej: 100"); //Solicito precio
+      precioBolsa = parseFloat(precioBolsa);
+    }
+
+    //Inicio operatoria por productos
+
+    switch (tipoProducto) 
+    {
+      case "arena":
+
+        contadorArena = contadorArena + cantidadBolsas; //Cant. bolsas de Arena
+        if (precioBolsa > caroArena) 
+        {
+          caroArena = caroArena + precioBolsa; //Valor acumulado de la compra de Arena para comparar el más caro
+        }
+        break;
+
+      case "cal":
+
+        contadorCal = contadorCal + cantidadBolsas; //Cant. Bolsas de Cal
+        if (precioBolsa > caroCal)
+        {
+          caroCal = caroCal + precioBolsa; //Valor acumulado de la compra de Cal para comparar el más caro
+        }
+        break;
+
+      case "cemento":
+
+        contadorCemento = contadorCemento + cantidadBolsas; //cant. de jabones comprados
+        if (precioBolsa > caroCemento)
+        {
+          caroCemento = caroCemento + precioBolsa; //Valor acumulado de la compra de Cemento para comparar el más caro
+        }
+        break;
+    }
+
+    respuesta = prompt("¿Desea Ingresar otro pedido de materiales?", "si/no");
+    respuesta = respuesta.toLowerCase();
+
+  }// fin del While
+
+  //a) El importe total a pagar , bruto sin descuento.
+
+  importeTotalBruto = (contadorArena*caroArena) + (contadorCal+caroCal) + (contadorCemento*caroCemento);
+
+  alert(`El Importe Total Burto es de $${importeTotalBruto} `)
+
+  //b) el importe total a pagar con descuento(solo si corresponde)
+
+  totalBolsas = contadorArena + contadorCal + contadorCemento; //calculo la cantidad total de bolsas
+
+  if ( totalBolsas > 30 ) 
+  {
+    descuento = 0.25;
+  } 
+  else 
+  {
+    if (totalBolsas > 10 && totalBolsas < 30) 
+    {
+      descuento = 0.10;
+    } 
+    else 
+    {
+      descuento = 0;
+    }
+  }
+
+  importeTotalDescuento = importeTotalBruto - (importeTotalBruto * descuento); //aplico descuento
+
+  alert(`El Importe Total con descuento es $${importeTotalDescuento}`); //informo valor final con descuento
+
+  //c) Informar el tipo con mas cantidad de bolsas.
+
+  if (contadorArena > contadorCal && contadorArena > contadorCemento)
+  {
+    alert(`El tipo de material con más cantidad es Arena con ${contadorArena} bolsas.`);
+  } 
+  else 
+  {
+    if (contadorCal > contadorArena && contadorCal > contadorCemento) 
+    {
+      alert(`El tipo de material con más cantidad es Cal con ${contadorCal} bolsas.`)
+    } 
+    else 
+    {
+      if (contadorCemento > contadorArena && contadorCemento > contadorCal) 
+      {
+        alert(`El tipo de material con más cantidad es Cemento con ${contadorCemento} bolsas.`)  
+      }
+    }
+    
+  }
+
+  //d) El tipo mas caro
+
+  if (caroArena > caroCal && caroArena > caroCemento)
+  {
+    alert(`El tipo de material más caro es Arena con $${caroArena}`);
+  } 
+  else 
+  {
+    if (caroCal > caroArena && caroCal > caroCemento) 
+    {
+      alert(`El tipo de material más caro es Cal con $${caroCal}`)
+    } 
+    else 
+    {
+      if (caroCemento > caroArena && caroCemento > caroCal) 
+      {
+        alert(`El tipo de material más caro es Cemento con un valor por bolsa de $${caroCemento}`)  
+      }
+    }  
+  }
+}//fin de la función.
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
@@ -1,4 +1,145 @@
+/* parcial 2020
+Ejercicio 03
+Hermann Federico */
+
 function mostrar()
 {
-	alert("tres");
-}
+	//declaro variables
+	let nombre;
+	let edad;
+	let sexo;
+	let estadoCivil;
+	let temperatura;
+	let temperaturaMayor;
+	let personaTemperatura;
+	let respuesta;
+	let contadorHombresSolteros;
+	let acumuladorEdadSolteros;
+	let promedioEdadSolteros;
+	let contadorHombresViudos;
+	let contadorViudosMayores;
+	let contadorMayoresTemperatura;
+
+
+	//valores iniciales
+	respuesta = "si";
+	contadorHombresSolteros = 0;
+	acumuladorEdadSolteros = 0;
+	contadorHombresViudos = 0;
+	contadorViudosMayores = 0;
+	temperaturaMayor = 0;
+	contadorMayoresTemperatura = 0;
+
+
+
+	//valido datos y opero loop
+
+	while (respuesta == "si") 
+	{
+		nombre = prompt(`Ingrese su nombre`); // Solicito nombre
+		nombre = nombre.toLowerCase();
+
+		while ( isNaN(nombre) == false ) 
+		{
+			nombre = prompt(`Error de Ingreso, Reingrese su nombre`); // Solicito nombre nuevamente.
+			nombre = nombre.toLowerCase();
+		}
+
+		edad = prompt(`Ingrese su edad:`, `ej: 40`); // Solicito edad
+		edad = parseInt(edad);
+
+		while ( isNaN(edad) == true ) 
+		{
+			edad = prompt(`Error de Ingreso, Reingrese su edad:`, `ej: 40`); // Solicito edad
+			edad = parseInt(edad);
+		}
+
+		sexo = prompt(`Ingrese su sexo:`, `f/m`); // Solicito sexo
+		sexo = sexo.toLowerCase();
+
+		while ( isNaN(sexo) == false ) 
+		{
+			sexo = prompt(`Error de Ingreso, Reingrese su sexo:`, `f/m`); // Solicito sexo
+			sexo = sexo.toLowerCase();
+		}
+
+		estadoCivil = prompt(`Ingrese el estado civil`, "soltero/a - casado/a - separado/a - viudo/a"); //Solicito estado civil
+		estadoCivil = estadoCivil.toLowerCase();
+
+		while ( isNaN(estadoCivil) == false || estadoCivil !="soltero" && estadoCivil !="soltera" && estadoCivil !="casado" && estadoCivil !="casada" && estadoCivil !="separado" && estadoCivil !="separada" && estadoCivil !="viudo" && estadoCivil !="viuda" ) 
+		{
+			estadoCivil = prompt(`Error de ingreso, Reingrese el estado civil:`, "soltero/a - casado/a - separado/a - viudo/a"); //Solicito estado civil
+		}
+
+		temperatura = prompt(`Ingrese la temperatura`, `ej: 36.7`); //solicito cantidad de productos
+		temperatura = parseFloat(temperatura);
+
+		while ( isNaN(temperatura) == true || temperatura < 30 || temperatura > 50)
+		{
+			temperatura = prompt(`Error de Ingreso, reingrese la temperatura`, `ej: 36.7`); //solicito cantidad de productos
+			temperatura = parseFloat(temperatura);
+		}
+
+		switch (estadoCivil) 
+		{
+			case "viudo":
+			case "viuda":
+				if (edad > 60) // calculo las personas mayores
+				{
+					contadorViudosMayores ++;
+				}
+				
+				if (sexo = "m") 
+				{
+					contadorHombresViudos ++; //cuento los hombres viudos
+				}
+				break;
+
+			case "soltero":
+				contadorHombresSolteros ++; //cuento los hombres solteros
+				acumuladorEdadSolteros = acumuladorEdadSolteros + edad; //acumulo la edad para obtener luego un promedio
+				break;
+		}
+
+		if (temperatura > temperaturaMayor) //busco el nombre de la persona con mayor temperatura
+		{
+			temperaturaMayor = temperatura;
+			personaTemperatura = nombre;
+		}
+
+		if (edad > 60 && temperatura > 38 ) //cuento los mayores de edad con temperatura superior a 38º
+		{
+			contadorMayoresTemperatura ++;
+		}
+
+		respuesta = prompt("¿Desea Ingresar otro Pasajero?", "si/no");
+		respuesta = respuesta.toLowerCase();
+
+		while ( respuesta != "si" && respuesta != "no" ) 
+		{
+			respuesta = prompt("Error, ¿Desea Ingresar otro Pasajero?", "si/no"); // Solicito respuesta nuevamente.
+			respuesta = respuesta.toLowerCase();
+		}
+
+	}// fin del while
+
+	// opero con el grupo de datos.
+
+	//a) El nombre de la persona con mas temperatura.
+	alert(`La persona con temperatura más elevada es "${personaTemperatura}" con "${temperaturaMayor}"`);
+
+	//b) Cuantos mayores de edad estan viudos
+	alert(`la cantidad de adultos mayores viudos es de "${contadorViudosMayores}"`);
+
+	//c) La cantidad de hombres que hay solteros o viudos.
+	alert(`La cantidad de Hombres Solteros es de "${contadorHombresSolteros}"`);
+	alert(`La cantidad de Hombres Viudos es de "${contadorHombresViudos}"`);
+
+	//cuantas personas de la tercera edad( mas de 60 años) , tienen mas de 38 de temperatura
+	alert(`La cantidad de personas mayores, con temperatura superior a 38º es de "${contadorMayoresTemperatura}"`);
+
+	//e) El promedio de edad entre los hombres solteros.
+	promedioEdadSolteros = acumuladorEdadSolteros / contadorHombresSolteros;
+	alert(`El promedio de edad de los hombres solteros es de "${promedioEdadSolteros.toFixed(0)}"`);
+		
+}//fin de la funcion
