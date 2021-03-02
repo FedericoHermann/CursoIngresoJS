1644bce4ebfc0dc923cc387bc10789f922295ba6
Federico Hermann <hermann.f.a@gmail.com>
2021-03-01 21:01:05 -0300

    For 07, funcionando

--- 6-InstruccionFor/jsIteracionesFor-07.js
+++ 6-InstruccionFor/jsIteracionesFor-07.js
@@ -1,7 +1,43 @@
 function mostrar()
 {
+	//declaro variables
+	let numero;
+	let divisores;
+	let i;
 
-	alert("ok");
+	//inicializo variables
+	divisores = 0;
+
+	//Pido dato de entrada original
+	numero = prompt("Ingrese Número");
+	numero = parseInt(numero);
+
+	//busco divisores
+	if (numero >0) 
+	{
+		for(i=1; i <= numero; i++ )
+		{
+			if (numero % i == 0) 
+			{
+				divisores ++;
+				console.log("divisor hallado" + i);
+			}
+		}
+	} 
+	else 
+	{
+		for(i=1; i >= numero; i-- )
+		{
+			if (numero % i == 0) 
+			{
+				divisores ++;
+				console.log("divisor hallado" + i);
+			}
+		}
+		
+	}
+		
+	console.log("La cantidad de divisores hallados es: " + divisores);
 
 
 }//FIN DE LA FUNCIÓN
